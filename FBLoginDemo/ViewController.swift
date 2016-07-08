1370505747aa05439b775398311b2acaa70733a5
giovramirez <giovramirez@gmail.com>
2016-07-07 20:49:32 -0500

    Initial Commit

---
+++ FBLoginDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FBLoginDemo
+//
+//  Created by Giovanny Ramirez on 07/07/16.
+//  Copyright © 2016 Giovanny Ramirez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
